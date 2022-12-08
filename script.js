2f675175c57c764fc26c03b4c91569940118339d
noceive <nocivescience@gmail.com>
2022-12-08 04:33:57 -0300

    segundo video

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const run_state=0;
+
 // características de juego
 let state={
     canvas: null,
@@ -9,13 +10,15 @@ let state={
     growing: 0,
     runState: run_state,
 }
-function drawPixel(){    
-    state.ctx.fillStyle='red';
-    state.ctx.fillRect(30,30,60,50);
+function drawPixel(color,x,y){    
+    state.ctx.fillStyle=color;
+    state.ctx.fillRect(10,10,x,y);
 }
 function draw(){
     state.ctx.clearRect(0,0,window.innerWidth,window.innerHeight);
-    drawPixel()
+    drawPixel('green',30,10)
+    drawPixel('red',20,10);
+    drawPixel('yellow',10,10);
 }
 function tick(){
     requestAnimationFrame(draw)
@@ -24,6 +27,8 @@ function tick(){
 window.onload=function(){
     state.canvas=document.getElementById('game');
     state.ctx=state.canvas.getContext('2d');
+    state.canvas.width=window.innerWidth;
+    state.canvas.height=window.innerHeight;
     window.onkeydown=function(e){
         console.log(e.key);
     }
